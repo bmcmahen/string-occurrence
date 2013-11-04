02bee3245eedc1145d55418985852061b41eb128
bmcmahen <ben.mcmahen@gmail.com>
2013-11-04 15:09:29 -0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+
+# string-occurrence
+
+  find all occurrences of a substring within a string, and return indexes
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install bmcmahen/string-occurrence
+
+## API
+
+```javascript
+var occurrence = require('string-occurrence');
+var results = occurrence('this is a string that repeats this', 'this');
+// results.length == 2
+```
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,26 @@
+var contains = require('contains-word');
+
+// Modelled after 'while': http://jsperf.com/count-string-occurrence-in-string
+
+/**
+ * Determine the indexes of each occurrance of a substring
+ * within a string.
+ * @param  {String} string    
+ * @param  {String} substring 
+ * @return {Array}           
+ */
+
+module.exports = function(string, substring){
+  var instances = [];
+  var pos = 0;
+  while (true){
+    var occurrence = contains(string, substring, pos);
+    if (occurrence) {
+      instances.push(occurrence);
+      pos = occurrence.end;
+    } else {
+      break;
+    }
+  }
+  return instances;
+};
